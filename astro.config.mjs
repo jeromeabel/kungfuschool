c7d5dac208a6aafd42c7d0c07e830764f6eb62c9
Jérôme Abel <contact@jeromeabel.net>
2023-11-17 11:56:12 +0100

    tool: add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jeromeabel.github.io', // Git Actions
+  site: 'https://jeromeabel.github.io',
+  // Git Actions
   base: '/kungfuschool',
+  integrations: [tailwind()]
 });
