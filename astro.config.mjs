63d1e53ad270c1afdc871136e4697604bfde49c2
Jérôme Abel <contact@jeromeabel.net>
2023-11-19 09:17:51 +0100

    project: change to npm, setup prettier and tw

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jeromeabel.github.io',
   // Git Actions
   base: '/kungfuschool',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
 });
