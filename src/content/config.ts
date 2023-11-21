3442999fb78c7eca3c621e63c1b70763e6397ab4
Jérôme Abel <contact@jeromeabel.net>
2023-11-21 21:03:04 +0100

    content: add courses collection and data

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,34 +2,48 @@ import { defineCollection, z } from 'astro:content';
 
 const schoolCollection = defineCollection({
 	type: 'data',
-	schema: () =>
-		z.object({
-			name: z.string(),
-			address: z.object({
-				street: z.string(),
-				zip: z.number(),
-				city: z.string(),
-				openstreetmap: z.string(),
-			}),
-			infos: z.object({
-				date: z.date(),
-				surface: z.number(),
-				studentsPerYear: z.number(),
-				medals: z.number(),
-			}),
-			telephone: z.string(),
-			email: z.string(),
-			schedules: z.string(),
-			socials: z.array(
-				z.object({
-					label: z.string(),
-					href: z.string(),
-					icon: z.string(),
-				}),
-			),
+	schema: z.object({
+		name: z.string(),
+		address: z.object({
+			street: z.string(),
+			zip: z.number(),
+			city: z.string(),
+			openstreetmap: z.string(),
 		}),
+		infos: z.object({
+			date: z.date(),
+			surface: z.number(),
+			studentsPerYear: z.number(),
+			medals: z.number(),
+		}),
+		telephone: z.string(),
+		email: z.string(),
+		schedules: z.string(),
+		socials: z.array(
+			z.object({
+				label: z.string(),
+				href: z.string(),
+				icon: z.string(),
+			}),
+		),
+	}),
+});
+
+const coursesCollection = defineCollection({
+	type: 'content',
+	schema: ({ image }) => {
+		z.object({
+			title: z.string(),
+			type: z.enum(['yearly', 'individual']),
+			description: z.string(),
+			cover: image(),
+			illustration: image(),
+			schedules: z.array(z.string()),
+		});
+	},
 });
 
 export const collections = {
 	school: schoolCollection,
+	course: coursesCollection,
 };
---
+++ src/content/course/combat/index.md
@@ -0,0 +1,14 @@
+---
+title: 'Combat'
+type: 'yearly'
+description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, maiores vero consequatur'
+cover: './cover.jpg'
+illustration: './combat.svg'
+schedules:
+  - 'lundi 18h-19h30'
+  - 'mardi 20h-21h30'
+---
+
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, maiores vero consequatur eos quia voluptatibus velit eius debitis. Facilis expedita quas impedit doloribus, eum veniam nam? Magnam provident numquam nesciunt?
+
+Distinctio quo asperiores, sint dolorem commodi illum nulla obcaecati! Ullam nisi ab atque quia sunt vitae quaerat libero iusto voluptas, inventore possimus fuga, quisquam quo distinctio expedita neque obcaecati non?
