1ad610901d5d73e2a31c9486763aa34d33dbc5fa
Jérôme Abel <contact@jeromeabel.net>
2024-01-02 18:09:20 +0100

    setup: upgrade to astro v4 and remove sitemap, astropb/md

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+// import sitemap from '@astrojs/sitemap';
+//sitemap()
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jeromeabel.github.io',
 	// Git Actions
 	base: '/kungfuschool',
-	integrations: [tailwind(), sitemap()],
+	integrations: [tailwind()],
 });
