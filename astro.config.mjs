4c4797da3f21b29b58e9b6ebdc086f4d00f1f566
Jérôme Abel <contact@jeromeabel.net>
2024-01-12 10:59:26 +0100

    tests: assetPrefix for generated Images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 // import sitemap from '@astrojs/sitemap';
-//sitemap()
+// sitemap()
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +10,8 @@ export default defineConfig({
 	// Git Actions
 	base: '/kungfuschool',
 	integrations: [tailwind()],
+	build: {
+		assets: 'assets',
+		assetsPrefix: 'https://jeromeabel.github.io/kungfuschool',
+	},
 });
