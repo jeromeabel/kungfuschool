5084c2a75a44aafa3bcac8de7a475e02c62bf38d
Jérôme Abel <contact@jeromeabel.net>
2024-01-12 11:03:56 +0100

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-// import sitemap from '@astrojs/sitemap';
-// sitemap()
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jeromeabel.github.io',
 	// Git Actions
 	base: '/kungfuschool',
-	integrations: [tailwind()],
+	integrations: [tailwind(), sitemap()],
 	build: {
 		assets: 'assets',
 		assetsPrefix: 'https://jeromeabel.github.io/kungfuschool',
